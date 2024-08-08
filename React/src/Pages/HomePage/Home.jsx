821f7f2ad6d189cee25f8d675cae5f49793721bf
Samir Jabre <73381924>
2024-08-08 04:07:51 +0300

    Fixed bug

--- React/src/Pages/HomePage/Home.jsx
+++ React/src/Pages/HomePage/Home.jsx
@@ -10,44 +10,44 @@ function Home({userInfo}) {
   const coursesId=[];
   const courses = [];
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.post('http://127.0.0.1:6969/enrollment/enrollmentbyid', {
-          userId
-        }, {
-          headers: {
-            'Content-Type': 'application/json',
-          }
-        });
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.post('http://127.0.0.1:6969/enrollment/enrollmentbyid', {
+  //         userId
+  //       }, {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         }
+  //       });
   
-        const data = response.data;
-        console.log(data);
+  //       const data = response.data;
+  //       console.log(data);
   
-        for (let i = 0; i < data.length; i++) {
-          coursesId.push(data[i].courseId);
-        }
+  //       for (let i = 0; i < data.length; i++) {
+  //         coursesId.push(data[i].courseId);
+  //       }
   
-        console.log(coursesId);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+  //       console.log(coursesId);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   };
   
-    fetchData();
-    // for (let i = 0; i < coursesId.length; i++){
-    //   axios.get(`http://127.0.0.1:6969/courses/${coursesId[i]}`)
-    //   .then((result)=>{console.log(result);
-    //   });
-    // }
-  }, [userId]);
+  //   fetchData();
+  //   // for (let i = 0; i < coursesId.length; i++){
+  //   //   axios.get(`http://127.0.0.1:6969/courses/${coursesId[i]}`)
+  //   //   .then((result)=>{console.log(result);
+  //   //   });
+  //   // }
+  // }, [userId]);
 
-  useEffect(async()=>{
-    for (let i = 0; i < coursesId.length; i++){
-      await axios.get(`http://127.0.0.1:6969/courses/${coursesId[i]}`)
-      .then((result)=>console.log(result));
-    }
-  },[coursesId])
+  // useEffect(async()=>{
+  //   for (let i = 0; i < coursesId.length; i++){
+  //     await axios.get(`http://127.0.0.1:6969/courses/${coursesId[i]}`)
+  //     .then((result)=>console.log(result));
+  //   }
+  // },[coursesId])
 
 
   return (
